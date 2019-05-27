56c6f4c91e1b37b7d7260c9387f465e032e300f8
AristarkhV <49162607+AristarkhV@users.noreply.github.com>
2019-05-27 20:44:29 +0300

    Update DoubleExpression.java

--- src/com/company/DoubleExpression.java
+++ src/com/company/DoubleExpression.java
@@ -1,4 +1,8 @@
 package com.company;
 
 public class DoubleExpression {
+  
+      public static boolean doubleExpression(double a, double b, double c) {
+        return (Math.abs((a + b) - c) < (1e-4));
+    }
 }
