94212121781487d3dc717fa70ce02a733c4bda7a
AristarkhV <49162607+AristarkhV@users.noreply.github.com>
2019-05-27 20:46:45 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        
+        FirstTask first = new FirstTask();
+        System.out.println(first.booleanExpression(false,false,true,true));
+
+        LeapYear count = new LeapYear();
+        System.out.println(count.leapYearCount(100));
+
+        DoubleExpression expr = new DoubleExpression();
+        System.out.println(expr.doubleExpression(0.1,0.1,0.2));
+
+        FlipBit flipBit = new FlipBit();
+        System.out.println(flipBit.flipBit(7, 2));// 0111 -> 0101
     }
 }
