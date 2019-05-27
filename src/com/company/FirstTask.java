e4b25870a2e216a236279fc5757c0d1765653e9c
AristarkhV <49162607+AristarkhV@users.noreply.github.com>
2019-05-27 20:45:18 +0300

    Update FirstTask.java

--- src/com/company/FirstTask.java
+++ src/com/company/FirstTask.java
@@ -1,4 +1,9 @@
 package com.company;
 
 public class FirstTask {
+  
+   public boolean booleanExpression(boolean a,boolean b,boolean c,boolean d){
+
+            return (((a & b) ^ (b & c) ^ (c & d)) ^ ((a & b & c & d)));
+        }
 }
