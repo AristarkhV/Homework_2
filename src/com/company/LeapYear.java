896aef59e1487bbe76ff9481d9f258f40409c603
AristarkhV <49162607+AristarkhV@users.noreply.github.com>
2019-05-27 20:45:44 +0300

    Update LeapYear.java

--- src/com/company/LeapYear.java
+++ src/com/company/LeapYear.java
@@ -1,4 +1,8 @@
 package com.company;
 
 public class LeapYear {
+  
+  public static int leapYearCount(int year) {
+        return ((int)(year / 4) - (int)(year / 100) + (int)(year / 400));
+    }
 }
