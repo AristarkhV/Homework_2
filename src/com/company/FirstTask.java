c847fedf9fe0b8783e3e97fdd7c8eed23fc64088
AristarkhV <49162607+AristarkhV@users.noreply.github.com>
2019-05-30 11:29:18 +0300

    Delete FirstTask.java

--- src/com/company/FirstTask.java
+++
@@ -1,6 +0,0 @@
-package com.company;
-public class FirstTask {
-   public boolean booleanExpression(boolean a,boolean b,boolean c,boolean d){
-            return (((a & b) ^ (b & c) ^ (c & d)) ^ ((a & b & c & d)));
-        }
-}
