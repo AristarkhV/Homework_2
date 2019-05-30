4d1e785c66ba1ece6eacce2a07706dbca47ef50d
AristarkhV <49162607+AristarkhV@users.noreply.github.com>
2019-05-30 11:28:59 +0300

    Delete LeapYear.java

--- src/com/company/LeapYear.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class LeapYear {
-  
-  public static int leapYearCount(int year) {
-        return ((int)(year / 4) - (int)(year / 100) + (int)(year / 400));
-    }
-}
